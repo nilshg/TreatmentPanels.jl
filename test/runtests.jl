7905189e95261dba54630d3b6ffbb1a6d13826c7
Nils <nils@gudat.uk>
2021-11-12 14:37:34 +0000

    Initial tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,17 @@
 using TreatmentPanels
 using Test
 
-@testset "TreatmentPanels.jl" begin
-    # Write your tests here.
+@testset "Input tests" begin
+    test_df = DataFrame(id = ['a', 'a', 'b', 'b'], period = [1, 2, 1, 2], value = 1.0:4.0)
+    
+    # Basic functionality
+    @test BalancedPanel(test_df, :id, :period, :value, ['a' => 2]) isa BalancedPanel
+    
+    # Column not present in data
+    @test_throws "" BalancedPanel(test_df, :wrong_var, :period, :value, ['a'=> 2])
+    @test_throws "" BalancedPanel(test_df, :id, :wrong_var, :value, ['a' => 2])
+    @test_throws "" BalancedPanel(test_df, :id, :period, :wrong_var, ['a' => 2])
+    
+    # Treatment assignment has nonexistent units/time periods
+    @test_throws "" BalancedPanel(test_df, :id, :period, :value, ['c' => 2])
 end
